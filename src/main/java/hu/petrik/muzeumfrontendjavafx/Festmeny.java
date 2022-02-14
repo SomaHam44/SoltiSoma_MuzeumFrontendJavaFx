1f0845a0ff8e47bc1d118079a8cceab755d65a0d
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2022-02-14 19:27:38 +0100

    Festmény osztály kialakítása

--- src/main/java/hu/petrik/muzeumfrontendjavafx/Festmeny.java
+++ src/main/java/hu/petrik/muzeumfrontendjavafx/Festmeny.java
@@ -1,4 +1,47 @@
 package hu.petrik.muzeumfrontendjavafx;
 
 public class Festmeny {
+    private int id;
+    private String cim;
+    private int ev;
+    private boolean kiallitva;
+
+    public Festmeny(int id, String cim, int ev, boolean kiallitva) {
+        this.id = id;
+        this.cim = cim;
+        this.ev = ev;
+        this.kiallitva = kiallitva;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCim() {
+        return cim;
+    }
+
+    public void setCim(String cim) {
+        this.cim = cim;
+    }
+
+    public int getEv() {
+        return ev;
+    }
+
+    public void setEv(int ev) {
+        this.ev = ev;
+    }
+
+    public boolean isKiallitva() {
+        return kiallitva;
+    }
+
+    public void setKiallitva(boolean kiallitva) {
+        this.kiallitva = kiallitva;
+    }
 }
