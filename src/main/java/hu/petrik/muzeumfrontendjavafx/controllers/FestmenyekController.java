d0ba393fe01c4c307a5a625f02b3ae99531cae62
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2022-02-14 19:52:27 +0100

    Festmény, szobor listázás kinézete

--- src/main/java/hu/petrik/muzeumfrontendjavafx/controllers/FestmenyekController.java
+++ src/main/java/hu/petrik/muzeumfrontendjavafx/controllers/FestmenyekController.java
@@ -1,6 +1,23 @@
 package hu.petrik.muzeumfrontendjavafx.controllers;
 
 import hu.petrik.muzeumfrontendjavafx.Controller;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 public class FestmenyekController extends Controller {
+    public TableView festmenyTable;
+    public TableColumn colCim;
+    public TableColumn colEv;
+    public TableColumn colKiallitva;
+
+    public void onHozzadasButtonClick(ActionEvent actionEvent) {
+    }
+
+    public void onModositasButtonClick(ActionEvent actionEvent) {
+    }
+
+    public void onTorlesButtonClick(ActionEvent actionEvent) {
+
+    }
 }
--- src/main/java/hu/petrik/muzeumfrontendjavafx/controllers/SzobrokController.java
+++ src/main/java/hu/petrik/muzeumfrontendjavafx/controllers/SzobrokController.java
@@ -1,6 +1,22 @@
 package hu.petrik.muzeumfrontendjavafx.controllers;
 
 import hu.petrik.muzeumfrontendjavafx.Controller;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 public class SzobrokController extends Controller {
+    public TableView szoborTable;
+    public TableColumn colSzemely;
+    public TableColumn colMagassag;
+    public TableColumn colAr;
+
+    public void onHozzadasButtonClick(ActionEvent actionEvent) {
+    }
+
+    public void onModositasButtonClick(ActionEvent actionEvent) {
+    }
+
+    public void onTorlesButtonClick(ActionEvent actionEvent) {
+    }
 }
