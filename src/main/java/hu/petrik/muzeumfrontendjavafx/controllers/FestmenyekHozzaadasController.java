34ad31bd6a066729044d1b2e40dc7ed9545c5d56
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2022-02-19 21:50:09 +0100

    Festmény módosítása és hozzáadása

--- src/main/java/hu/petrik/muzeumfrontendjavafx/controllers/FestmenyekHozzaadasController.java
+++ src/main/java/hu/petrik/muzeumfrontendjavafx/controllers/FestmenyekHozzaadasController.java
@@ -1,6 +1,61 @@
 package hu.petrik.muzeumfrontendjavafx.controllers;
 
 import hu.petrik.muzeumfrontendjavafx.Controller;
+import hu.petrik.muzeumfrontendjavafx.Festmeny;
+import hu.petrik.muzeumfrontendjavafx.MuzeumApi;
+import hu.petrik.muzeumfrontendjavafx.Szobor;
+import javafx.event.ActionEvent;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
 
 public class FestmenyekHozzaadasController extends Controller {
+    public Spinner<Integer> inputEv;
+    public CheckBox inputKiallitva;
+    public TextField inputCim;
+
+    public void onHozzaadButtonClick(ActionEvent actionEvent) {
+        String cim = inputCim.getText().trim();
+        boolean kiallitva = inputKiallitva.isSelected();
+        int ev = 0;
+        if (cim.isEmpty()) {
+            alert("A cím megadása kötelező!");
+            return;
+        }
+
+        if (cim.length() < 3) {
+            alert("A cím  neve 3 karakternél hosszabb kell, hogy legyen!");
+            return;
+        }
+
+        try {
+            ev = inputEv.getValue();
+        } catch (NullPointerException ex) {
+            alert("Az év megadása kötelező");
+            return;
+        } catch (Exception e) {
+            alert("Az év csak 100 és 3000 közötti szám lehet");
+            return;
+        }
+
+        if (ev < 100 || ev > 3000) {
+            alert("Az év csak 100 és 3000 közötti szám lehet");
+            return;
+        }
+
+
+
+        try {
+            Festmeny ujFestmeny = new Festmeny(0, cim, ev, kiallitva);
+            Festmeny letrehozottFestmeny = MuzeumApi.festmenyHozzaadasa(ujFestmeny);
+            if (letrehozottFestmeny != null) {
+                alert("A festmény hozzáadása sikeres");
+            } else {
+                alert("A festmény hozzáadása sikertelen");
+            }
+        } catch (Exception e) {
+            hibaKiiro(e);
+        }
+
+    }
 }
--- src/main/java/hu/petrik/muzeumfrontendjavafx/controllers/FestmenyekModositasController.java
+++ src/main/java/hu/petrik/muzeumfrontendjavafx/controllers/FestmenyekModositasController.java
@@ -2,9 +2,20 @@ package hu.petrik.muzeumfrontendjavafx.controllers;
 
 import hu.petrik.muzeumfrontendjavafx.Controller;
 import hu.petrik.muzeumfrontendjavafx.Festmeny;
+import hu.petrik.muzeumfrontendjavafx.MuzeumApi;
+import hu.petrik.muzeumfrontendjavafx.Szobor;
+import javafx.event.ActionEvent;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
 
 public class FestmenyekModositasController extends Controller {
 
+    public Spinner<Integer> inputEv;
+    public CheckBox inputKiallitva;
+    public TextField inputCim;
     private Festmeny modositando;
 
     public Festmeny getModositando() {
@@ -13,5 +24,62 @@ public class FestmenyekModositasController extends Controller {
 
     public void setModositando(Festmeny modositando) {
         this.modositando = modositando;
+        ertekekBeallitasa();
+    }
+
+    private void ertekekBeallitasa() {
+        inputCim.setText(modositando.getTitle());
+        inputEv.getValueFactory().setValue(modositando.getYear());
+        inputKiallitva.setSelected(modositando.isOn_display());
+
+    }
+
+    public void onModositasButtonClick(ActionEvent actionEvent) {
+        String cim = inputCim.getText().trim();
+        int ev = 0;
+        boolean kiallitva = inputKiallitva.isSelected();
+        if (cim.isEmpty()) {
+            alert("Cím megadása kötelező!");
+            return;
+        }
+
+        if (cim.length() < 3) {
+            alert("A cím  3 karakternél hosszabb kell, hogy legyen!");
+            return;
+        }
+
+
+        try {
+            ev = inputEv.getValue();
+        } catch (NullPointerException ex) {
+            alert("Az év megadása kötelező");
+            return;
+        } catch (Exception e) {
+            alert("Az év csak 100 és 3000 közötti szám lehet");
+            return;
+        }
+
+        if (ev < 100 || ev > 3000) {
+            alert("Az év csak 100 és 3000 közötti szám lehet");
+            return;
+        }
+
+
+        modositando.setTitle(cim);
+        modositando.setYear(ev);
+        modositando.setOn_display(kiallitva);
+
+        try {
+            Festmeny modositott = MuzeumApi.festmenyModositasa(modositando);
+            if (modositott != null) {
+                alertWait("Sikeres módosítás");
+                this.stage.close();
+            } else {
+                alert("Sikertelen módosítás");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
